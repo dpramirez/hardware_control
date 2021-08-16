86ad38334b772911f30d3d677119434233f70d6a
dpramirez <dpramirez@miuandes.cl>
2021-08-15 20:35:48 -0400

    actualizacion de status door

--- control_puertas/create_instruction/create_instruction.py
+++ control_puertas/create_instruction/create_instruction.py
@@ -12,7 +12,7 @@ def create():
                     "open":b"\x80\x01\x01\x11\x91",
                     "close": b"\x80\x01\x01\x00\x80"
                 },
-                "status": b"\x80\x01\x01\x22\xA2"
+                "status": b"\x80\x01\x01\x33\xb3"
             },
             "2": {
                 "open":b"\x8A\x01\x02\x11\x98",
@@ -20,7 +20,8 @@ def create():
                 "feedback_status":{
                     "open":b"",
                     "close": b""
-                }
+                },
+                "status": b"\x80\x01\x02\x33\xb0"
             },
             
             "3": {
@@ -29,7 +30,8 @@ def create():
                 "feedback":{
                     "open":b"",
                     "close": b""
-                }
+                },
+                "status": b"\x80\x01\x03\x33\xb1"
             },
             "4": {
                 "open":b"\x8A\x01\x04\x11\x9e",
@@ -37,7 +39,8 @@ def create():
                 "feedback":{
                     "open":b"",
                     "close": b""
-                }
+                },
+                "status": b"\x80\x01\x04\x33\xb6"
             },
             "5": {
                 "open":b"\x8A\x01\x05\x11\x9f",
@@ -45,7 +48,8 @@ def create():
                 "feedback":{
                     "open":b"",
                     "close": b""
-                }
+                },
+                "status": b"\x80\x01\x05\x33\xb7"
             },                        
             "6": {
                 "open":b"\x8A\x01\x06\x11\x9c",
@@ -53,13 +57,14 @@ def create():
                 "feedback":{
                     "open":b"",
                     "close": b""
-                }
+                },
+                "status": b"\x80\x01\x06\x33\xb4"
             }            
         }
     }
 
 
-    dbfile = open("/home/pi/Desktop/DTS/save.p", "wb")
+    dbfile = open("save.p", "wb")
     pickle.dump(instruction, dbfile)
     dbfile.close()
 
@@ -72,3 +77,6 @@ def byte_xor(bar1, bar2):
 #print(byte_xor(byte_xor(byte_xor(b'\x8A', b'\x01'), number_door), b'\x11'))
 
 create()
+
+number_door = b'\x06'
+print(byte_xor(byte_xor(byte_xor(b'\x80', b'\x01'), number_door), b'\x33'))
