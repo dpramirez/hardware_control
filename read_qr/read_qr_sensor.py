fa07b681e3c1a3981fb727075f4dff20fc391bfc
dpramirez <dpramirez@miuandes.cl>
2021-08-16 09:47:35 -0400

    update qr code, two mods

--- read_qr/read_qr_sensor.py
+++ read_qr/read_qr_sensor.py
@@ -5,53 +5,59 @@ import socket
 import sys
 
 
-# configure the serial connections (the parameters differs on the device you are connecting to)
-ser = Serial(
-    port='/dev/ttyACM1',
-    baudrate=9600
-)
 
-ser.isOpen()
 
 def serial_read_qr():
-
+    """
+    Conexion directa del QR a la espera 
+    """
+    ser = Serial(
+        port='/dev/ttyACM1',
+        baudrate=9600
+    )
+    ser.isOpen()
     print(ser.is_open)
-
     print("connected to: " + ser.portstr)
-    
-    
     while True:
         print(ser.read(22))
 
     
+def socket_comunication():
+    # configure the serial connections (the parameters differs on the device you are connecting to)
+    ser = Serial(
+        port='/dev/ttyACM1',
+        baudrate=9600
+    )
 
+    ser.isOpen()
 
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Connect the socket to the port where the server is listening
+    server_address = ('localhost', 10000)
+    print('connecting to {} port {}'.format(*server_address))
+    sock.connect(server_address)
 
-# Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
-print('connecting to {} port {}'.format(*server_address))
-sock.connect(server_address)
+    while True:
+        try:
 
-while True:
-    try:
+            # Send data
+            message = ser.read(22)
+            print('sending {!r}'.format(message))
+            sock.sendall(message)
 
-        # Send data
-        message = ser.read(22)
-        print('sending {!r}'.format(message))
-        sock.sendall(message)
+            # Look for the response
+            amount_received = 0
+            amount_expected = len(message)
 
-        # Look for the response
-        amount_received = 0
-        amount_expected = len(message)
+            while amount_received < amount_expected:
+                data = sock.recv(16)
+                amount_received += len(data)
+                print('received {!r}'.format(data))
 
-        while amount_received < amount_expected:
-            data = sock.recv(16)
-            amount_received += len(data)
-            print('received {!r}'.format(data))
+        finally:
+            print('closing socket')
+            #sock.close()
 
-    finally:
-        print('closing socket')
-        #sock.close()
+serial_read_qr()            
