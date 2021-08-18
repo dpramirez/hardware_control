8bd79c36aafa36dd1796b3e8979f30f668b39650
dpramirez <dpramirez@miuandes.cl>
2021-08-18 17:02:24 -0400

    update repositiry

---
+++ test_camera.py
@@ -0,0 +1,19 @@
+
+from camera.camera import *
+from threading import Thread
+import time
+import threading
+
+rtsp_stream_link = 2
+video_stream_widget = RTSPVideoWriterObject(rtsp_stream_link)
+
+endRec = False
+t1 = threading.Thread(target = video_stream_widget.startRecord) 
+t1.start() 
+time.sleep(5)
+define(True)    
+
+print('..............')
+print(endRec)
+
+         
