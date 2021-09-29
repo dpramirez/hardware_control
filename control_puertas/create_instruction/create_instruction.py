ace112a8e3a541ffd4af9a58cf2427d9f43c6e61
dpramirez <dpramirez@miuandes.cl>
2021-09-29 14:39:10 -0300

    Update create_instruction.py
    actualizacion de instrucciones

--- control_puertas/create_instruction/create_instruction.py
+++ control_puertas/create_instruction/create_instruction.py
@@ -62,129 +62,129 @@ def create():
             }, # Desde aca los feedback no estan buenos hay que arreglarlos                       
             "7": {
                 "open":b"\x8A\x01\x07\x11\x9d",
-                "response": b"",
+                "response": b"\x80\x01\x07\x11\x97",
                 "feedback_status":{
-                    "open": b"\x80\x01\x06\x11\x96",
-                    "close": b"\x80\x01\x06\x00\x87"
+                    "open": b"\x80\x01\x07\x11\x97",
+                    "close": b"\x80\x01\x07\x00\x86"
                 },
-                "status": b"\x80\x01\x06\x33\xb4"
+                "status": b"\x80\x01\x07\x33\xb5"
             },                        
             "8": {
                 "open":b"\x8A\x01\x08\x11\x92",
                 "response": b"",
                 "feedback_status":{
-                    "open": b"\x80\x01\x06\x11\x96",
-                    "close": b"\x80\x01\x06\x00\x87"
+                    "open": b"\x80\x01\x08\x11\x98",
+                    "close": b"\x80\x01\x08\x00\x89"
                 },
-                "status": b"\x80\x01\x06\x33\xb4"
+                "status": b"\x80\x01\x08\x33\xba"
             },                        
             "9": {
                 "open":b"\x8A\x01\x09\x11\x93",
                 "response": b"",
                 "feedback_status":{
-                    "open": b"\x80\x01\x06\x11\x96",
-                    "close": b"\x80\x01\x06\x00\x87"
+                    "open": b"\x80\x01\x09\x11\x99",
+                    "close": b"\x80\x01\x09\x00\x88"
                 },
-                "status": b"\x80\x01\x06\x33\xb4"
+                "status": b"\x80\x01\x09\x33\xbb"
             },                        
             "10": {
                 "open":b"\x8A\x01\x0A\x11\x90",
                 "response": b"",
                 "feedback_status":{
-                    "open": b"\x80\x01\x06\x11\x96",
-                    "close": b"\x80\x01\x06\x00\x87"
+                    "open": b"\x80\x01\x0A\x11\x9a",
+                    "close": b"\x80\x01\x0A\x00\x8b"
                 },
-                "status": b"\x80\x01\x06\x33\xb4"
+                "status": b"\x80\x01\x0A\x33\xb8"
             },                        
             "11": {
                 "open":b"\x8A\x01\x0B\x11\x91",
                 "response": b"",
                 "feedback_status":{
-                    "open": b"\x80\x01\x06\x11\x96",
-                    "close": b"\x80\x01\x06\x00\x87"
+                    "open": b"\x80\x01\x0B\x11\x9b",
+                    "close": b"\x80\x01\x0B\x00\x8a"
                 },
-                "status": b"\x80\x01\x06\x33\xb4"
+                "status": b"\x80\x01\x0B\x33\xb9"
             },                        
             "12": {
                 "open":b"\x8A\x01\x0C\x11\x96",
                 "response": b"",
                 "feedback_status":{
-                    "open": b"\x80\x01\x06\x11\x96",
-                    "close": b"\x80\x01\x06\x00\x87"
+                    "open": b"\x80\x01\x0C\x11\x9c",
+                    "close": b"\x80\x01\x0C\x00\x8d"
                 },
-                "status": b"\x80\x01\x06\x33\xb4"
+                "status": b"\x80\x01\x0C\x33\xbe"
             },                        
             "13": {
                 "open":b"\x8A\x01\x0D\x11\x97",
                 "response": b"",
                 "feedback_status":{
-                    "open": b"\x80\x01\x06\x11\x96",
-                    "close": b"\x80\x01\x06\x00\x87"
+                    "open": b"\x80\x01\x0D\x11\x9d",
+                    "close": b"\x80\x01\x0D\x00\x8c"
                 },
-                "status": b"\x80\x01\x06\x33\xb4"
+                "status": b"\x80\x01\x0D\x33\xbf"
             },                        
             "14": {
                 "open":b"\x8A\x01\x0E\x11\x94",
                 "response": b"",
                 "feedback_status":{
-                    "open": b"\x80\x01\x06\x11\x96",
-                    "close": b"\x80\x01\x06\x00\x87"
+                    "open": b"\x80\x01\x0E\x11\x9e",
+                    "close": b"\x80\x01\x0E\x00\x8f"
                 },
-                "status": b"\x80\x01\x06\x33\xb4"
+                "status": b"\x80\x01\x0E\x33\xbc"
             },                        
             "15": {
                 "open":b"\x8A\x01\x0F\x11\x95",
                 "response": b"",
                 "feedback_status":{
-                    "open": b"\x80\x01\x06\x11\x96",
-                    "close": b"\x80\x01\x06\x00\x87"
+                    "open": b"\x80\x01\x0F\x11\x9f",
+                    "close": b"\x80\x01\x0F\x00\x8e"
                 },
-                "status": b"\x80\x01\x06\x33\xb4"
+                "status": b"\x80\x01\x0F\x33\xbd"
             },                        
             "16": {
                 "open":b"\x8A\x01\x10\x11\x8A",
                 "response": b"",
                 "feedback_status":{
-                    "open": b"\x80\x01\x06\x11\x96",
-                    "close": b"\x80\x01\x06\x00\x87"
+                    "open": b"\x80\x01\x10\x11\x80",
+                    "close": b"\x80\x01\x10\x00\x91"
                 },
-                "status": b"\x80\x01\x06\x33\xb4"
+                "status": b"\x80\x01\x10\x33\xa2"
             },                        
             "17": {
                 "open":b"\x8A\x01\x11\x11\x8b",
                 "response": b"",
                 "feedback_status":{
-                    "open": b"\x80\x01\x06\x11\x96",
-                    "close": b"\x80\x01\x06\x00\x87"
+                    "open": b"\x80\x01\x11\x11\x81",
+                    "close": b"\x80\x01\x11\x00\x90"
                 },
-                "status": b"\x80\x01\x06\x33\xb4"
+                "status": b"\x80\x01\x11\x33\xa3"
             },                        
             "18": {
                 "open":b"\x8A\x01\x12\x11\x88",
                 "response": b"",
                 "feedback_status":{
-                    "open": b"\x80\x01\x06\x11\x96",
-                    "close": b"\x80\x01\x06\x00\x87"
+                    "open": b"\x80\x01\x12\x11\x82",
+                    "close": b"\x80\x01\x12\x00\x93"
                 },
-                "status": b"\x80\x01\x06\x33\xb4"
+                "status": b"\x80\x01\x12\x33\xa0"
             },                        
             "19": {
                 "open":b"\x8A\x01\x13\x11\x89",
                 "response": b"",
                 "feedback_status":{
-                    "open": b"\x80\x01\x06\x11\x96",
-                    "close": b"\x80\x01\x06\x00\x87"
+                    "open": b"\x80\x01\x13\x11\x83",
+                    "close": b"\x80\x01\x13\x00\x92"
                 },
-                "status": b"\x80\x01\x06\x33\xb4"
+                "status": b"\x80\x01\x13\x33\xa1"
             },                        
             "20": {
                 "open":b"\x8A\x01\x14\x11\x8e",
                 "response": b"",
                 "feedback_status":{
-                    "open": b"\x80\x01\x06\x11\x96",
-                    "close": b"\x80\x01\x06\x00\x87"
+                    "open": b"\x80\x01\x14\x11\x84",
+                    "close": b"\x80\x01\x14\x00\x95"
                 },
-                "status": b"\x80\x01\x06\x33\xb4"
+                "status": b"\x80\x01\x14\x33\xa6"
             }                         
         }
     }
@@ -199,9 +199,13 @@ def create():
 def byte_xor(bar1, bar2):
         return bytes([_a ^ _b for _a, _b in zip(bar1, bar2)])
 
-number_door = b'\x14'
-print(byte_xor(byte_xor(byte_xor(b'\x8A', b'\x01'), number_door), b'\x11'))
+number_door = b'\x0E'
+#print(byte_xor(byte_xor(byte_xor(b'\x80', b'\x01'), number_door), b'\x00'))
 
+#for i in range(1, 21):
+    #number_door = i.to_bytes(1, byteorder='big')
+    #print(i, byte_xor(byte_xor(byte_xor(b'\x80', b'\x01'), number_door), b'\x33'))
+    
 
 create()
 
